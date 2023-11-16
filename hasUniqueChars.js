c53af502a0b8916e77715920e0f31e54bc97ce85
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-11-16 13:21:46 -0600

    has unique characters code challenge

---
+++ hasUniqueChars.js
@@ -0,0 +1,12 @@
+// Write your code below
+function hasUniqueChars(word) {
+  let uniqueChars = new Set([])
+  for (let i = 0; i < word.length; i++) {
+    uniqueChars.add(word[i])
+  }
+  return uniqueChars.size === word.length
+}
+
+console.log(hasUniqueChars('Lastly'))
+
+hasUniqueChars('Billy')
